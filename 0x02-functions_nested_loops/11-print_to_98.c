d9e9a22588b97ce9866b65ca405f0f3449378412
ntando <ntando064@gmail.com>
2023-01-01 10:13:35 -0800

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,19 +9,19 @@
 
 void print_to_98(int n)
 {
-	if (n<=98){
-		for (;n<=98;n++){
-			if (n==98){
+	if (n <= 98){
+		for (; n <= 98; n++){
+			if (n == 98){
 				printf("%d", n);
 				printf("\n");
 				break;
 			} else{
-				printf("%d", n);
+				printf("%d, ", n);
 			}
 		}
 	}else{
-		for (;n>=98;n--){
-			if(n==98){
+		for (; n >= 98; n--){
+			if(n == 98){
 				printf("%d", n);
 				printf("\n");
 				break;
